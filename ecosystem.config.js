90a915322134025344584189129d0835cb9c3234
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-08-27 13:26:27 +0530

    update ecosystem.config.js in service

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,36 +1,50 @@
 module.exports = {
     apps: [
+
         {
-            name: "config-server",
-            cwd: "/home/chaminduchirantha2/lms-cloud-project-platform",
-            script: "java",
-            args: "-jar config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+            name: "cloud-sql-auth-proxy",
+            script: "./cloud-sql-proxy",
+            args: "project-5cb0d26c-1f1d-4c90-862:asia-southeast1:mysql?port=3306 project-5cb0d26c-1f1d-4c90-862:asia-southeast1:postgresql?port=5432 --private-ip",
+            out_file: "./logs/cloud-sql-proxy-out.log",
+            error_file: "./logs/cloud-sql-proxy-error.log",
             autorestart: true,
-            restart_delay: 5000,
-            out_file: "./logs/config-server-out.log",
-            error_file: "./logs/config-server-error.log"
+            restart_delay: 5000
         },
 
         {
-            name: "service-registry",
-            cwd: "/home/chaminduchirantha2/lms-cloud-project-platform",
-            script: "java",
-            args: "-jar service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
+            name: "book-service",
+            script: "bash",
+            args: "-c 'sleep 30 && exec java -jar book-service/target/books-service-0.0.1-SNAPSHOT.jar'",
+            out_file: "./logs/book-service-out.log",
+            error_file: "./logs/book-service-error.log",
+            instances: 2,
+            exec_mode: "fork",
             autorestart: true,
-            restart_delay: 10000,
-            out_file: "./logs/service-registry-out.log",
-            error_file: "./logs/service-registry-error.log"
+            restart_delay: 10000
         },
 
         {
-            name: "api-gateway",
-            cwd: "/home/chaminduchirantha2/lms-cloud-project-platform",
-            script: "java",
-            args: "-jar api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+            name: "user-service",
+            script: "bash",
+            args: "-c 'sleep 30 && exec java -jar user-service/target/user-service-0.0.1-SNAPSHOT.jar'",
+            out_file: "./logs/user-service-out.log",
+            error_file: "./logs/user-service-error.log",
+            instances: 2,
+            exec_mode: "fork",
             autorestart: true,
-            restart_delay: 10000,
-            out_file: "./logs/api-gateway-out.log",
-            error_file: "./logs/api-gateway-error.log"
+            restart_delay: 10000
+        },
+
+        {
+            name: "borrowing-service",
+            script: "bash",
+            args: "-c 'sleep 30 && exec java -jar borrowing-service/target/borrowing-service-0.0.1-SNAPSHOT.jar'",
+            out_file: "./logs/borrowing-service-out.log",
+            error_file: "./logs/borrowing-service-error.log",
+            instances: 2,
+            exec_mode: "fork",
+            autorestart: true,
+            restart_delay: 10000
         }
     ]
 };
